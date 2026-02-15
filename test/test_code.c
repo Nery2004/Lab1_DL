f374dc6a3a41dea8da262c563e977a2875be59df
Nery2004 <josemolinatics@gmail.com>
2026-02-15 11:59:23 -0600

    Add lexer implementation and test files for token recognition

---
+++ test/test_code.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    int a = 10;
+    float b = 3.14;
+    double c = 1.2e-3;
+    int h = 0xFF;
+
+    // Comparaciones y logicos
+    if (a >= 10 && h != 0) {
+        printf("Valor: %d\n", a);
+    }
+
+    /* Comentario multilinea
+       con operadores: == != <= >= && || !
+    */
+
+    char* s = "hola\tmundo\n";
+    return 0;
+}
